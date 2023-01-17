94a649fae455eb8442c04c6110e66ee0f97b934b
白雾三语 <32354856+baiwusanyu-c@users.noreply.github.com>
2023-01-17 10:27:09 +0800

    chore: format code template (#28)
    * chore: format code antd-react template
    
    * chore: format code element-vue template
    
    * chore: format code antd-vue template

--- template/react-vite/antd/src/store/index.ts
+++ template/react-vite/antd/src/store/index.ts
@@ -1,11 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { counterSlice } from './features/counterSlice'
+// eslint-disable-next-line import/no-named-as-default
+import counterSlice from './features/counterSlice'
 
 // configureStore创建一个redux数据
 const store = configureStore({
   // 合并多个Slice
   reducer: {
-    counter: counterSlice.reducers,
+    counter: counterSlice,
   },
 })
 
