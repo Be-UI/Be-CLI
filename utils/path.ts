8402c0b19843886044a6fb497cc2236df593cead
wangxiaoShow <1090211589@qq.com>
2023-01-17 21:18:31 +0800

    fix: fix path error

--- utils/path.ts
+++ utils/path.ts
@@ -1,33 +1,28 @@
 import * as path from 'path'
-import { fileURLToPath } from 'url'
 import { cwd } from 'node:process'
 
-const __filenameNew: string = fileURLToPath(import.meta.url)
-export const __dirnameNew = path.dirname(__filenameNew)
-
 export const templatePath = {
-  element: path.resolve(process.env.ENV === 'BECLIDEV' ? cwd() : __dirnameNew, 'template/vue-vite/element'),
-  antd: path.resolve(process.env.ENV === 'BECLIDEV' ? cwd() : __dirnameNew, 'template/vue-vite/antd'),
-  antdReact: path.resolve(process.env.ENV === 'BECLIDEV' ? cwd() : __dirnameNew, 'template/react-vite/antd'),
-  windicss: path.resolve(process.env.ENV === 'BECLIDEV' ? cwd() : __dirnameNew, 'template/vue-vite/windicss'),
-  unocss: path.resolve(process.env.ENV === 'BECLIDEV' ? cwd() : __dirnameNew, 'template/vue-vite/unocss'),
-  vitest: path.resolve(process.env.ENV === 'BECLIDEV' ? cwd() : __dirnameNew, 'template/unit-test/vitest-vue'),
-  vitestReact: path.resolve(process.env.ENV === 'BECLIDEV' ? cwd() : __dirnameNew, 'template/unit-test/vitest-react'),
-  vitestLib: path.resolve(process.env.ENV === 'BECLIDEV' ? cwd() : __dirnameNew, 'template/unit-test/vitest-lib'),
-  jest: path.resolve(process.env.ENV === 'BECLIDEV' ? cwd() : __dirnameNew, 'template/unit-test/jest-vue'),
-  jestReact: path.resolve(process.env.ENV === 'BECLIDEV' ? cwd() : __dirnameNew, 'template/unit-test/jest-react'),
-  jestLib: path.resolve(process.env.ENV === 'BECLIDEV' ? cwd() : __dirnameNew, 'template/unit-test/jest-lib'),
-  tsup: path.resolve(process.env.ENV === 'BECLIDEV' ? cwd() : __dirnameNew, 'template/lib-template/gulp-tsup'),
-  rollup: path.resolve(process.env.ENV === 'BECLIDEV' ? cwd() : __dirnameNew, 'template/lib-template/gulp-rollup'),
-  browser: path.resolve(process.env.ENV === 'BECLIDEV' ? cwd() : __dirnameNew, 'template/lib-play'),
-  bms: path.resolve(process.env.ENV === 'BECLIDEV' ? cwd() : __dirnameNew, 'template/other/bms'),
-  puvue: path.resolve(process.env.ENV === 'BECLIDEV' ? cwd() : __dirnameNew, 'template/other/pure-vue'),
-  pureact: path.resolve(process.env.ENV === 'BECLIDEV' ? cwd() : __dirnameNew, 'template/other/pure-react'),
+  element: path.resolve(cwd(), 'template/vue-vite/element'),
+  antd: path.resolve(cwd(), 'template/vue-vite/antd'),
+  antdReact: path.resolve(cwd(), 'template/react-vite/antd'),
+  windicss: path.resolve(cwd(), 'template/vue-vite/windicss'),
+  unocss: path.resolve(cwd(), 'template/vue-vite/unocss'),
+  vitest: path.resolve(cwd(), 'template/unit-test/vitest-vue'),
+  vitestReact: path.resolve(cwd(), 'template/unit-test/vitest-react'),
+  vitestLib: path.resolve(cwd(), 'template/unit-test/vitest-lib'),
+  jest: path.resolve(cwd(), 'template/unit-test/jest-vue'),
+  jestReact: path.resolve(cwd(), 'template/unit-test/jest-react'),
+  jestLib: path.resolve(cwd(), 'template/unit-test/jest-lib'),
+  tsup: path.resolve(cwd(), 'template/lib-template/gulp-tsup'),
+  rollup: path.resolve(cwd(), 'template/lib-template/gulp-rollup'),
+  browser: path.resolve(cwd(), 'template/lib-play'),
+  bms: path.resolve(cwd(), 'template/other/bms'),
+  puvue: path.resolve(cwd(), 'template/other/pure-vue'),
+  pureact: path.resolve(cwd(), 'template/other/pure-react'),
 }
 export const libTemplateName = {
   tsup: 'template-node-tsup',
   rollup: 'template-node-rollup',
 }
 
-const cliPackageFilePath = process.env.ENV === 'BECLIDEV' ? 'package.json' : '../package.json'
-export const cliPackagePath = path.resolve(process.env.ENV === 'BECLIDEV' ? cwd() : __dirnameNew, cliPackageFilePath)
+export const cliPackagePath = path.resolve(cwd(), 'package.json')
