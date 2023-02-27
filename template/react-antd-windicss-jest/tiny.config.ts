ecd7b151b22eafba67b6eeafd3d3941657b23aad
baiwusanyu-c <740132583@qq.com>
2023-02-27 11:25:37 +0800

    chore: 增加模板

---
+++ template/react-antd-windicss-jest/tiny.config.ts
@@ -0,0 +1,10 @@
+import { defineTinyConfig } from 'easy-tinypng-cli/utils'
+
+export default defineTinyConfig({
+  configs: [
+    {
+      targetDir: './src/assets/images',
+    },
+  ],
+  APIKey: 'kZgn8pxfdjQjKFmf2StLq7CY4TqMgs0T',
+})
---
+++ template/react-antd-windicss-vitest/vitest.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    disabled: true,
+  },
+  test: {
+    clearMocks: true,
+    environment: 'jsdom',
+    transformMode: {
+      web: [/\.[jt]sx$/],
+    },
+  },
+})
