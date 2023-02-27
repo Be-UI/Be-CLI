41a00770c8632670f38bbc38d0d15ce4118b6784
白雾三语 <32354856+baiwusanyu-c@users.noreply.github.com>
2023-02-27 23:24:44 +0800

    chore: 打包脚本编写

--- build-script/build-pkg.js
+++ build-script/build-pkg.js
@@ -1,4 +1,5 @@
 import fs from 'fs-extra'
+
 async function buildPkg() {
   const pkg = await fs.readJson('./package.json')
   const content = JSON.parse(JSON.stringify(pkg))
@@ -7,4 +8,37 @@ async function buildPkg() {
   Reflect.deleteProperty(content.scripts, 'husky:step2')
   await fs.writeJson('./dist/package.json', content, { spaces: 2 })
 }
-buildPkg()
+
+async function buildBin() {
+  const file = await fs.readFile('./be-cli/bin/index.ts')
+  file.toString().replace('index', 'index.js')
+  const contextStr = file.toString().replace('index', 'index.js')
+  await fs.outputFileSync('./dist/bin/index.js', contextStr)
+}
+
+async function buildTemplate() {
+   const filterFile = (src) => {
+    if ((src.includes('auto-imports.d')
+        || src.includes('components.d')
+        || src.includes('.git')
+        || src.includes('node_modules')
+        || src.includes('.idea')) && src.includes('.gitignore'))
+      return false
+
+    return true
+  }
+  await fs.copySync('./template', './dist/template', { filter: filterFile })
+  // 删除 node_modules
+  await fs.readdir('./dist/template',(err,files)=>{
+    files.forEach(val=>{
+      val !== '.DS_Store' && fs.remove(`./dist/template/${val}/node_modules`)
+    })
+  })
+}
+
+const run  = async () =>{
+  await buildTemplate()
+  await buildPkg()
+  await buildBin()
+}
+await run ()
