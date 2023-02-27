31d53e5217a47a2c25fbe8b23875d0c6df0049c7
白雾三语 <32354856+baiwusanyu-c@users.noreply.github.com>
2023-02-27 22:01:17 +0800

    update: 完成 other 类型

--- be-cli/runtime/runtime-other/index.ts
+++ be-cli/runtime/runtime-other/index.ts
@@ -1,34 +1,25 @@
 import ora from 'ora'
 import chalk from 'chalk'
 import fs from 'fs-extra'
-import { OTHERTYPE, filterFile, templatePath } from '../../utils'
+import { filterFile } from '../../utils'
 import { readPackageJson, writePackageJson } from '../read-write-package'
-import { addBaseUnitTest, addUnitTestDeps } from '../add-unit-test'
 import type { IOtherOption } from '../../utils'
 
 export async function runRuntimeOther(option: IOtherOption) {
   const {
     projectName,
     projectPath,
-    otherType,
+    templateDir,
   } = option
   const spinner = ora('Loading').start()
   try {
     spinner.color = 'blue'
-    await fs.copySync(templatePath[otherType as keyof typeof templatePath], projectPath, { filter: filterFile })
-
-    // TODO: 读取 package.json ，修改名称
+    // 复制模板项目
+    await fs.copySync(templateDir, projectPath, { filter: filterFile })
+    // 读取 package.json ，修改名称
     const packageJson = await readPackageJson(option)
     packageJson.name = projectName
-
-    // 添加单元测试
-    // await addBaseUnitTest(packageJson, option, otherType === OTHERTYPE.PUREACT ? 'React' : '')
-    // if (otherType === OTHERTYPE.PUREACT)
-    //   packageJson = addUnitTestDeps(packageJson, option, 'React')
-    // else
-    //   packageJson = addUnitTestDeps(packageJson, option, 'Vue')
-
-    // TODO: 写入package.json
+    // 写入package.json
     await writePackageJson(projectPath, packageJson)
 
     spinner.text = chalk.greenBright.bold(`\ncreate project <${projectName}> success !`)
