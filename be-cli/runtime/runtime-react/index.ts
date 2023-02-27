e680563e8f86b5f5460b30a1d25273b07540280f
baiwusanyu-c <740132583@qq.com>
2023-02-27 14:35:43 +0800

    update: 修改项目模板的单测依赖

--- be-cli/runtime/runtime-react/index.ts
+++ be-cli/runtime/runtime-react/index.ts
@@ -19,17 +19,17 @@ export const runRuntimeReact = async(option: IViteProjOption) => {
     spinner.color = 'blue'
     await fs.copySync(templatePath[`${uiLibType}React` as keyof typeof templatePath], projectPath, { filter: filterFile })
 
-    // 读取 package.json ，修改名称
+    // TODO: 读取 package.json ，修改名称
     let packageJson = await readPackageJson(option)
     packageJson.name = projectName
 
     // 设置原子css
-    await addAtomCss(packageJson, option, 'tsx')
+   // await addAtomCss(packageJson, option, 'tsx')
+   // // 添加单元测试
+   // await addBaseUnitTest(packageJson, option, 'React')
+   // packageJson = addUnitTestDeps(packageJson, option, 'React')
 
-    // 添加单元测试
-    await addBaseUnitTest(packageJson, option, 'React')
-    packageJson = addUnitTestDeps(packageJson, option, 'React')
-    // 写入package.json
+    // TODO: 写入package.json
     await writePackageJson(projectPath, packageJson)
 
     spinner.text = chalk.greenBright.bold(`\ncreate project <${projectName}> success !`)
